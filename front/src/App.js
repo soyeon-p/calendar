c3002b666c07d0ceb70ff2b3c95689e3cd5bebcc
2jaebbang <2jaebbang@naver.com>
2022-01-16 01:15:15 +0900

    feat: 캘린더 추가

--- front/src/App.js
+++ front/src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/core';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Components/Layout';
-import { theme } from './Styles/theme';
+import { theme } from './Styles/Theme';
 import Home from './Pages/Home';
 import Dust from './Pages/Dust';
 import Weather from './Pages/Weather';
 import Corona from './Pages/Corona';
 import { RecoilRoot } from 'recoil';
+import Calendar from './Components/Calendar/Calendar';
 
 function App() {
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <Layout>
+          <Calendar/>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Weather" element={<Weather />} />
