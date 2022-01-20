b18359247b0a035b976312cb2991c3b39176ea4c
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-01-20 17:07:35 +0900

    feat: 지역에 따른 날씨API 가져오기

--- back/routes/weather.js
+++ back/routes/weather.js
@@ -4,15 +4,26 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const router = express.Router();
+let cityCode;
 
-const weatherURL = `http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?dataType=JSON&serviceKey=${process.env.API_KEY}&numOfRows=10&pageNo=1&dataCd=ASOS&dateCd=DAY&startDt=20210112&endDt=20210118&stnIds=108`;
-router.get('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    const {data} = await axios.get(weatherURL);
-    res.json(data);
+    cityCode = req.body.cityCode;
   } catch (err) {
     console.error(err);
     next(err);
+  }
+});
+
+router.get('/city', async (req, res, next) => {
+  try {
+    const weatherURL = `http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?dataType=JSON&serviceKey=${process.env.API_KEY}&numOfRows=10&pageNo=1&dataCd=ASOS&dateCd=DAY&startDt=20210112&endDt=20210118&stnIds=${cityCode}`;
+
+    const { data } = await axios.get(weatherURL);
+    console.log(weatherURL);
+    res.json(data);
+  } catch (err) {
+    console.error(err);
   }
 });
 
--- front/src/Components/Weather/Filter.js
+++ front/src/Components/Weather/Filter.js
@@ -2,35 +2,90 @@ import {
   Box,
   Button,
   FormControl,
-  Input,
   InputLabel,
   MenuItem,
   Select,
   Typography,
 } from '@material-ui/core';
+import axios from 'axios';
 import React, { useCallback, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { weatherDataState } from '../../Recoil/atoms';
 import { useStyles } from '../../Styles/styles';
+
+const cityList = [
+  { name: '속초', code: 90 },
+  { name: '서울', code: 108 },
+  { name: '인천', code: 112 },
+  { name: '수원', code: 119 },
+  { name: '서산', code: 129 },
+  { name: '청주', code: 131 },
+  { name: '포항', code: 138 },
+  { name: '대구', code: 143 },
+  { name: '전주', code: 146 },
+  { name: '울산', code: 152 },
+  { name: '창원', code: 155 },
+  { name: '광주', code: 156 },
+  { name: '부산', code: 159 },
+  { name: '제주', code: 184 },
+  { name: '세종', code: 239 },
+  { name: '대전', code: 258 },
+];
 
 const Filter = () => {
   const classes = useStyles();
-  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useRecoilState(weatherDataState);
+  const [cityCode, setCityCode] = useState('');
 
   const onChangeCity = useCallback((e) => {
-    setCity(e.target.value);
+    setCityCode(e.target.value);
   }, []);
+
+  const onSubmit = useCallback((e) => {
+    const postData = async () => {
+      try {
+        await axios.post('http://localhost:3065/weather', {
+          cityCode,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const fetchData = async () => {
+      const {
+        data: {
+          response: {
+            body: {
+              items: { item: weatherData },
+            },
+          },
+        },
+      } = await axios('http://localhost:3065/weather/city');
+      setWeatherData(weatherData);
+    };
+
+    e.preventDefault();
+    postData();
+    fetchData();
+    },
+    [cityCode, setWeatherData]
+  );
 
   return (
     <Box className={classes.box}>
       <Typography variant="subtitle1">2022-01-15</Typography>
       <Typography variant="h5">Weather</Typography>
       <div className={classes.filter}>
-        <form>
+        <form onSubmit={onSubmit}>
           <FormControl className={classes.formControl}>
             <InputLabel>지역</InputLabel>
-            <Select value={city} onChange={onChangeCity}>
-              <MenuItem value="서울">서울</MenuItem>
-              <MenuItem value="경기">경기</MenuItem>
-              <MenuItem value="울산">울산</MenuItem>
+            <Select value={cityCode} onChange={onChangeCity}>
+              {cityList.map((city) => (
+                <MenuItem key={city.code} value={city.code}>
+                  {city.name}
+                </MenuItem>
+              ))}
             </Select>
           </FormControl>
           <Button type="submit">Select</Button>
