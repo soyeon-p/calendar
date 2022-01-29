915545115074ac37b184f0147e49b6a0d66efc78
parksoyeon <soyeon1056@naver.com>
2022-01-29 21:42:29 +0900

    nodejs

--- config/config.js
+++ config/config.js
@@ -11,14 +11,14 @@ module.exports = {
       "test": {
         "username": "root",
         "password": process.env.SEQUELIZE_PASSWORD,
-        "database": "database_test",
+        "database": "nodejs",
         "host": "127.0.0.1",
         "dialect": "mysql"
       },
       "production": {
         "username": "root",
         "password": process.env.SEQUELIZE_PASSWORD,
-        "database": "database_production",
+        "database": "nodejs",
         "host": "127.0.0.1",
         "dialect": "mysql"
       }
